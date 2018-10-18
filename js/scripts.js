676895239604b9fed234a282e676f442a7740694
g00gol <jackey2870@gmail.com>
2018-10-18 18:44:27 -0500

    Changes to Code

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//Smooth Scroll
+//Nav
+$(document).ready(function(){
+  $(".sub-nav > a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+
+
+
+
+//AOS
+$(function() {
+  AOS.init();
+});
+
+$(window).on('load', function() {
+  AOS.refresh();
+});
