e76da4de33e5e014d7ea6f342147b1cf6bd7f8cc
g00gol <jackey2870@gmail.com>
2018-10-30 13:20:31 -0500

    Changes to Code

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,21 @@ $(document).ready(function(){
   });
 });
 
+//toTop
+$(document).ready(function(){
+  $(".toTop").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
 
 
 
